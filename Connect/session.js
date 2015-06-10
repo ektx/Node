c33148fe7513a70bd0969ec87b890792334910fc
kings <530675800@qq.com>
2015-06-10 10:50:47 +0800

    Express session

---
+++ Connect/session.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var session = require('express-session');
+
+var app = express();
+
+app.use(session({
+	secret:'Keyboard cat',
+	cookie: {
+		maxAge: 60000
+	}
+}));
+
+
+app.use(function(req, res, next) {
+	var sess = req.session;
+	if (sess.views) {
+		sess.views++;
+		res.setHeader('Content-Type', 'text/html; charset=utf-8');
+		res.write('<p>views: ' + sess.views + '</p>');
+		res.write('<p>express in: ' + (sess.cookie.maxAge / 1000) + 's</p>');
+		res.end();
+	} else {
+		sess.views = 1;
+		res.end('Welcome to the session demo. refresh!!')
+	}
+});
+
+app.listen(3000)
