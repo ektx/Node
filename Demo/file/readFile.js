2b6b64388b3af9dc1ecb204ee5e094cb9ac78a2c
ektx <ektx1989@gmail.com>
2015-09-01 06:56:27 +0800

    fs readFile eg:

---
+++ Demo/file/readFile.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+/*
+	fs.readFile(filename, [encoding], [callback(err, data)])
+
+	@filename	文件路径
+	@options	可选,设置 encoding,编码格式
+	@callback	回调,传3个参数,err和文件内容
+	
+*/ 
+
+
+fs.readFile('fs.js', 'utf-8', function(err, data) {
+	if (err) return next(err);
+
+	console.log(data)
+})
