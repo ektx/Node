1903f78aaa71b02c231f72a0bc2af57b82d21624
kings <530675800@qq.com>
2016-08-22 19:54:40 +0800

    示例

--- Command/commandTool/index.js
+++ Command/commandTool/index.js
@@ -7,62 +7,66 @@ var program = require('commander');
 var chalk = require('chalk');
 var progressBar = require('progress');
 
-program.arguments('<file>')
+program.arguments('<file> <file1>')
 .option('-u, --username <username>', 'The user to authenticate as')
 .option('-p, --password <password>', 'The user\'s password')
-.action(function(file) {
+.action(function(file, f2) {
 	co(function *() {
-		var username = yield prompt(chalk.green('username: '));
-		var password = yield prompt.password(chalk.green('password: '));
+		// var username = yield prompt(chalk.green('username: '));
+		// var password = yield prompt.password(chalk.green('password: '));
 
 		// var fileSize = fs.statSync(file).size;
-		var fileSize = 0;
-		file.forEach(function(val) {
-			fileSize += fs.statSync(val).size;
-			var fileStream = fs.createReadStream(file);
-		});
-		
-		// console.log(fileStream)
+		console.log('--', file)
+		console.log('--', f2)
+		file.split(' ').forEach(function(val) {
 
-		var barOptions = {
-			complete: '\u001b[42m \u001b[0m',
-			incomplete: '\u001b[41m \u001b[0m',
-			width: 30,
-			total: fileSize,
-			clear: false
-		};
+			console.log('-=-=',val, '---')
+			var fileSize = fs.statSync(val).size;
 
-		var bar = new progressBar(' 上传文件进度 [:bar] :total :percent :elapseds :etas', barOptions);
+			var fileStream = fs.createReadStream(val);
+			
+			fileStream.on('data', function(chunk) {
+				bar.tick(chunk.length)
+			});
 
-		fileStream.on('data', function(chunk) {
-			bar.tick(chunk.length)
-		});
+			var barOptions = {
+				complete: '\u001b[42m \u001b[0m',
+				incomplete: '\u001b[41m \u001b[0m',
+				width: 30,
+				total: fileSize,
+				clear: false
+			};
 
-		request.post('http://localhost:3000/profile')
-				.auth(username, password)
-				.attach('avatar', fileStream)
-				.field('name', username)
-				.field('password', password)
-				.end(function(err, res) {
-					if (!err && res.ok) {
-						console.log(chalk.bold.cyan('Status:') + res.status);
-						console.log('Text: %s', res.text);
-						process.exit(0)						
-					}
+			var bar = new progressBar(' 上传文件进度 [:bar] :total :percent :elapseds :etas', barOptions);
+	
+			request
+			.post('http://localhost:3000/profile')
+			// .auth(username, password)
+			.attach('avatar', fileStream)
+			// .attach('avatar', fileStream)
+			// .field('name', username)
+			// .field('password', password)
+			.end(function(err, res) {
+				if (!err && res.ok) {
+					console.log(chalk.bold.cyan('Status:') + res.status);
+					console.log('Text: %s', res.text);
+					// process.exit(0)						
+				}
 
-					var errorMessage;
-					if (res && res.status === 401) {
-						errorMessage = 'Authentication failed! Bad username/password';
-					} else if (err) {
-						errorMessage = err;
-					} else {
-						errorMessage = res.text;
-					}
+				var errorMessage;
+				if (res && res.status === 401) {
+					errorMessage = 'Authentication failed! Bad username/password';
+				} else if (err) {
+					errorMessage = err;
+				} else {
+					errorMessage = res.text;
+				}
 
-					console.error(chalk.red(errorMessage));
-					process.exit(1)
-				})
-	});
+				console.error(chalk.red(errorMessage));
+				// process.exit(1)
+			});
+		}); // End forEach
+	})
 })
 .parse(process.argv)
 
--- Command/commandTool/t.js
+++ Command/commandTool/t.js
@@ -2,22 +2,41 @@ var ProgressBar = require('progress');
 
 // Demonstrates the use of custom tokens
 
-var list = [
-  'image01.jpg', 'image02.jpg', 'image03.jpg', 'image04.jpg', 'image05.jpg',
-  'image06.jpg', 'image07.jpg', 'image08.jpg', 'image09.jpg', 'image10.jpg'
-]
+// var list = [
+//   'image01.jpg', 'image02.jpg', 'image03.jpg', 'image04.jpg', 'image05.jpg',
+//   'image06.jpg', 'image07.jpg', 'image08.jpg', 'image09.jpg', 'image10.jpg'
+// ]
 
-var bar = new ProgressBar(':percent eta: :eta downloading :current/:total :file', {
-  total: list.length
-})
+// var bar = new ProgressBar(':percent eta: :eta downloading :current/:total :file', {
+//   total: list.length
+// })
 
-var id = setInterval(function (){
-  bar.tick({
-    'file': list[bar.curr]
-  })
-  if (bar.complete) {
-    clearInterval(id)
+// var id = setInterval(function (){
+//   bar.tick({
+//     'file': list[bar.curr]
+//   })
+//   if (bar.complete) {
+//     clearInterval(id)
+//   }
+// }, 500)
+
+
+
+var bar = new ProgressBar('  progress [:bar] :percent :etas', {
+    complete: '='
+  , incomplete: ' '
+  , width: 40
+  , total: 100
+});
+
+var i = 0, steps = [0.1, 0.25, 0.6, 0.8, 0.4, 0.5, 0.6, 0.2, 0.8, 0.7];
+
+(function next() {
+  if (i >= steps.length) {
+  } else {
+    bar.update(steps[i++]);
+    setTimeout(next, 500);
   }
-}, 500)
+})();
 
 // https://github.com/visionmedia/node-progress/blob/master/examples/customtokens.js
