520060dea81e19954656d48d21951fab7c696c1b
kings <530675800@qq.com>
2017-05-09 22:55:37 +0800

    use require.js

---
+++ BaBel/amd.js
@@ -0,0 +1,7 @@
+define(['main.js'], function (_main) {
+
+	_main.hello()
+  return {
+  	add: function(x, y) { return x+y}
+  }
+});
--- BaBel/core.js
+++ BaBel/core.js
@@ -1,5 +1,7 @@
 let babel = require('babel-core');
 
-babel.transformFile('demo.js', function(err, result) {
-	console.log( result )
+babel.transformFile('demo.js', {
+  plugins: ["transform-es2015-modules-amd"]
+}, function(err, result) {
+	console.log( result.code )
 })
