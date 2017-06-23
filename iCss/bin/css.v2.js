6c15f2e0acfc986433ad58cc13a46ffbb5836c92
kings <530675800@qq.com>
2017-06-23 18:21:29 +0800

    U

--- iCss/bin/css.v2.js
+++ iCss/bin/css.v2.js
@@ -6,14 +6,47 @@ const dirFiles = require('./dirFiles');
 
 const SAVE_CSS_SPACE = {};
 
-function getImportCss (data) {
-	let result = {};
+function getImportCss (originPath, arr) {
+	let newArr = [];
 
-	if ( /[\n\r]/.test(val) ) {
-		_obj.comment = val.match(/\/\*.*\*\//)[0]
-	} else {
-		_obj.comment = ''
+	for (let val of arr) {
+
+		let _obj = {};
+
+		if ( /[\n\r]/.test(val) ) {
+			_obj.comment = val.match(/\/\*.*\*\//)[0]
+		} else {
+			_obj.comment = ''
+		}
+
+		val = val.match(/@im.+['"$]/gi)[0];
+		_obj.path = val.slice(13, val.length - 1);
+
+		_obj.resolve = path.resolve(originPath, _obj.path);
+
+		try {
+
+			let removeRelativePoint = new RegExp(`(\.{2}\/){${index}}`, 'g');
+
+			_obj.inner = 
+`\n\r/* ==================================
+${_obj.comment ? _obj.comment.slice(2, _obj.comment.length -2) : "无"}
+${_obj.path} 
+================================== */\n\r` + 
+				fs.readFileSync(_obj.resolve, 'utf8')
+				.replace(removeRelativePoint, '../')
+				.replace(/@charset\s('|")utf-8('|");/i, '')
+				.replace(/[\n\r]{3,}/,'')
+
+		} catch(err) {
+			_obj.inner = '';
+			_obj.error = err
+		}
+
+		newArr.push(_obj)
 	}
+
+	return newArr; 
 }
 
 function css(options) {
@@ -60,9 +93,11 @@ function css(options) {
 
 
 	let doThisFile = (data) => {
+		console.log(data)
 		let result = ['@charset "utf-8";'];
 		let originDirPath = path.dirname( options.file );
 		let allImportFiles = getImportCss(originDirPath, data.match(/(\/\*.+[\n\r])?@import.+;/gi ) );
+		console.log('xxx', allImportFiles )
 
 		allImportFiles.forEach( (val, index, array) => {
 			result.push(val.inner)
@@ -74,10 +109,10 @@ function css(options) {
 				.replace( /@charset\s('|")utf-8('|");/i, '')
 
 
-		if (callback) callback( result.join('')+data )		
+		console.log( result.join('')+data )		
 	}
 
-	doThisFile( SAVE_CSS_SPACE[inputDirName][options.file] )
+	doThisFile( SAVE_CSS_SPACE[inputDirName][options.file].originData )
 
 }
 
