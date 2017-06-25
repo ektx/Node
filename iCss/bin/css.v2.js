70709bbdbea71b55be44f1f876a6b97c5c8fd6f4
kings <530675800@qq.com>
2017-06-25 17:20:41 +0800

    修改结构

--- iCss/bin/css.v2.js
+++ iCss/bin/css.v2.js
@@ -3,11 +3,13 @@ const path = require('path');
 
 // 获取指定目录下所有的文件组件
 const dirFiles = require('./dirFiles');
-
+// 保存样式
 const SAVE_CSS_SPACE = {};
 
-function getImportCss (originPath, arr) {
+function getImportCss (data, parentFilePath) {
 	let newArr = [];
+
+	let arr = data.match(/(\/\*.+[\n\r])?@import.+;/gi )
 
 	for (let val of arr) {
 
@@ -16,32 +18,13 @@ function getImportCss (originPath, arr) {
 		if ( /[\n\r]/.test(val) ) {
 			_obj.comment = val.match(/\/\*.*\*\//)[0]
 		} else {
-			_obj.comment = ''
+			_obj.comment = '无'
 		}
 
 		val = val.match(/@im.+['"$]/gi)[0];
 		_obj.path = val.slice(13, val.length - 1);
 
-		_obj.resolve = path.resolve(originPath, _obj.path);
-
-		try {
-
-			let removeRelativePoint = new RegExp(`(\.{2}\/){${index}}`, 'g');
-
-			_obj.inner = 
-`\n\r/* ==================================
-${_obj.comment ? _obj.comment.slice(2, _obj.comment.length -2) : "无"}
-${_obj.path} 
-================================== */\n\r` + 
-				fs.readFileSync(_obj.resolve, 'utf8')
-				.replace(removeRelativePoint, '../')
-				.replace(/@charset\s('|")utf-8('|");/i, '')
-				.replace(/[\n\r]{3,}/,'')
-
-		} catch(err) {
-			_obj.inner = '';
-			_obj.error = err
-		}
+		_obj.resolve = path.resolve(path.dirname(parentFilePath), _obj.path);
 
 		newArr.push(_obj)
 	}
@@ -51,38 +34,39 @@ ${_obj.path}
 
 function css(options) {
 
-	let inputDirName = path.dirname(options.file);
 
-	let getAllCssPath = dirFiles(inputDirName, true);
-
-	if (getAllCssPath in SAVE_CSS_SPACE) {
-		console.log('此目录已经读取过!')
+	if (options.file in SAVE_CSS_SPACE) {
+		console.log('此文件已经读取过!')
 	} else {
+		// 得到父级地址
+		let inputDirName = path.dirname(options.file);
 
-		SAVE_CSS_SPACE[inputDirName] = {};
+		// 读取此文件所在父级目录下所有的文件
+		let getAllCssPath = dirFiles(inputDirName, true);
 
 		getAllCssPath.forEach( val => {
-			try {
 
-				if (val.type === 'file') {
+			// 对文件进行读取
+			if (val.type === 'file' && path.extname(val.name) === '.css') {
 
-					if (!(val.path in SAVE_CSS_SPACE[inputDirName])) {
+				// 此文件未被读取在内存中
+				if (!(val.path in SAVE_CSS_SPACE)) {
 
+					try {
 						let data = fs.readFileSync(val.path, 'utf8');
 						
-						SAVE_CSS_SPACE[inputDirName][val.path] = {
+						SAVE_CSS_SPACE[val.path] = {
 							originData: data
 						}
+					} catch (err) {
 
-					} else {
-						console.log(val.path+' 已经存在!')
+						console.log('读取以下文件时错误:\n', val.path);
 					}
 
+				} else {
+					console.log(val.path+' 已经存在!')
 				}
 
-			} catch (err) {
-
-				console.log('Error:', val.path);
 			}
 
 		})
@@ -91,28 +75,42 @@ function css(options) {
 		
 	}
 
+	/*
+		处理css
+		--------------------------------------
+		1.得到 import 所有内容
+		2.得到除去了 import 和 @charset 的内容
+	*/
+	let doThisCssFile = (filePath) => {
 
-	let doThisFile = (data) => {
-		console.log(data)
+		// 已经处理过
+		if (SAVE_CSS_SPACE[options.file].clearData) {
+			console.log( filePath, '已经处理过了')
+			return;
+		}
+
 		let result = ['@charset "utf-8";'];
 		let originDirPath = path.dirname( options.file );
-		let allImportFiles = getImportCss(originDirPath, data.match(/(\/\*.+[\n\r])?@import.+;/gi ) );
-		console.log('xxx', allImportFiles )
+		let dataParent = SAVE_CSS_SPACE[filePath];
+		let data = dataParent.originData;
 
-		allImportFiles.forEach( (val, index, array) => {
-			result.push(val.inner)
-		})
+		dataParent.import = getImportCss( data, filePath );
 
-		data = data
+
+		// allImportFiles.forEach( (val, index, array) => {
+		// 	result.push(val.inner)
+		// })
+
+		dataParent.clearData = data
 				.replace( /(\/\*.+[\n\r])?@import.+;/gi, '')
 				.replace( /[\r\n]{2,}/g, '\r\n' )
-				.replace( /@charset\s('|")utf-8('|");/i, '')
+				.replace( /@charset\s('|")utf-8('|");/i, '');
 
+		console.log('xxx', dataParent )
 
-		console.log( result.join('')+data )		
 	}
 
-	doThisFile( SAVE_CSS_SPACE[inputDirName][options.file].originData )
+	doThisCssFile( options.file )
 
 }
 
