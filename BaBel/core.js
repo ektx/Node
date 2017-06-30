205ef9c754df9b427fed5bd409370b6cc055ca70
kings <530675800@qq.com>
2017-06-30 18:10:53 +0800

    修改为es6

--- BaBel/core.js
+++ BaBel/core.js
@@ -2,6 +2,6 @@ let babel = require('babel-core');
 
 babel.transformFile('demo.js', {
   plugins: ["transform-es2015-modules-amd"]
-}, function(err, result) {
+}, (err, result) => {
 	console.log( result.code )
 })
