75ca7629a297fd3e0dd7038d7f7da41d800af738
kings <530675800@qq.com>
2017-09-21 20:44:04 +0800

    细节修改

--- iTool/bin/i.js
+++ iTool/bin/i.js
@@ -2,45 +2,83 @@
 const fs = require('fs');
 const path = require('path');
 
+const ITS_CACHE = {};
 
 function ijs(filePath) {
 
-	let fileInner = getFileInner(filePath);
+	let that;
+	let fileStat = fs.statSync(filePath);
 
-	let getModsArr = fileInner.match(/{{(\W|.)+?}}/g);
+	console.log('1.处理文件信息')
+	if (filePath in ITS_CACHE) {
 
-	// console.log(filePath, getModsArr);
+		if (fileStat.mtime > ITS_CACHE[filePath].mtime) {
+			console.log('file has changed!')
+		}
 
-	getModsArr.forEach(val=> {
+	} else {
+
+		ITS_CACHE[filePath] = {};
+		that = ITS_CACHE[filePath];
+
+		// 读取文件信息
+		that.stat = fileStat;
+
+	}
+
+	// 原始内容
+	that.origin = getFileInner(filePath);
+
+	console.log('get label')
+	let labelArr = that.origin.match(/<its-\w+/g);
+
+	labelArr = new Set(labelArr);
+
+	if (!('labels' in that)) that.labels = {};
+
+	labelArr.forEach(val => {
+debugger
+		let labelName = val.replace(/<its-/gi, '');
+		let valExp = val+'>((\\w|.|\\n|\\t)+?)<\/'+val.substr(1)+'>';
+		let valInner = getExpInner(valExp, that.origin);
+		let options = '';
+
+		// 添加容器
+		if (!that.labels[labelName]) that.labels[labelName] = {};
 		
-		let inner = val.substr(2, val.length -4);
-		let getPath = val.match(/include\(('|")(.+)('|")/)[2];
+		// 如果内部有js
+		if (/<script\s+options>/.test(valInner)) {
+			debugger
+			let _script = '<script\\s+options>((\\w|.|\\n|\\t)+?)<\/script>';
+			options = getExpInner(_script, valInner)
+			valInner = valInner.replace(_script, '')
 
-		// 获取传值
-		let getDataStr = inner.match(/{(\W|.)+?}/)[0];
-		// 处理成 json
-		let getData = getDataStr ? eval( '('+ inner.match(/{(\W|.)+?}/)[0] + ')') : {};
-
-		let modPath = setModPath(filePath, getPath);
+			that.labels[labelName].options = eval(`(${options})`)
+		}
 
 
-		console.log( getFileInner(modPath) )
+		that.labels[labelName].name = labelName;
+		that.labels[labelName].inner = valInner;
+
+
 	})
 
+
+	console.log( that )
 }
 
 function getFileInner (filePath) {
 	return fs.readFileSync(filePath, 'utf8')
 }
 
-function setModPath (mainPath, modPath) {
-	let result = path.resolve(mainPath, `./../${modPath}`);
+function getExpInner (exp, data, art = '') {
+	let _exp = new RegExp(exp, art);
+	let _inner = data.match(_exp);
 
-	if (!result.endsWidth('.its')) {
-		result += '.its'
-	}
+	_inner = !!_inner ? _inner[1] : '';
 
-	return result
+	return _inner;
 }
+
 
 module.exports = ijs
