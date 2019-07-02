2be1baf95710253f5a122afe6fe9c0d87c8d3064
ektx <530675800@qq.com>
2019-07-02 19:40:38 +0800

    文档调整

--- Async/async/async.js
+++ Async/async/async.js
@@ -1,5 +1,10 @@
+/*
+	基础示例
+	用于模拟异步的效果。
+*/
 let sleep = function(time) {
 	return new Promise( (resolve, reject) => {
+		console.log('sleep...')
 		setTimeout(()=> {
 			resolve()
 		}, time)
@@ -7,7 +12,7 @@ let sleep = function(time) {
 };
 
 let start = async function () {
-	// 在这里whet起来就和同步代码效果一般
+	// 在这里 await 起来就和同步代码效果一般
 	console.log('start');
 	await sleep(3000);
 	console.log('end')
---
+++ Async/readme.md
@@ -0,0 +1,5 @@
+# Async
+
+这里是原生功能与插件实现的功能示例的集合。
+
+- [async.js](https://caolan.github.io/async/v3/)
