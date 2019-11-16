577e77c17eecaac6f5900268413935f7e430698c
wuming <530675800@qq.com>
2019-11-16 13:55:22 +0800

    监听文件示例

--- Chokidar/index.js
+++ Chokidar/index.js
@@ -1,7 +1,20 @@
 const chokidar = require('chokidar')
+const path = require('path')
 
 chokidar.watch('.', {
-	ignored: /(^|[\/\\])\../,
+	ignored: filePath => {
+		console.log(filePath, path.extname(filePath))
+
+
+		if (filePath.includes('node_modules')) {
+			return true
+		}
+		console.log('>>>> .')
+		if (path.basename(filePath).startsWith('.') && filePath.length > 1) {
+			return true
+		}
+	},
+	
 	// persistent: true
 }).on('all', (event, path) => {
 	console.log(event, path)
